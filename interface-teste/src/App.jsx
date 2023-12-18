5c6d7cbf3325cc63f77c6b79f3e0a358ce558780
Murilo Lima Matioli <murilo.matioli@pmisa.net>
2023-12-18 10:48:32 -0300

    Side bar iniciada

--- interface-teste/src/App.jsx
+++ interface-teste/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+//import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <SideBar />
     </div>
   );
 }
