7021cae5f855d729830568cc3901b9bc9c5e7762
Murilo Lima Matioli <murilo.matioli@pmisa.net>
2024-01-24 11:08:40 -0300

    .

--- src/signupPages/Documentos.jsx
+++ src/signupPages/Documentos.jsx
@@ -6,25 +6,7 @@ function DocumentosPage(){
         <>
             <div className={styles.wrapper}>
                 <h2 className={styles.pageTitle}>Documentos e dados</h2>
-                <form action="/upload" method="post" enctype="multipart/form-data">
-                <div className={styles.mainContent}>
-                    <div className={styles.documentWrapper}>
-                        <p className={styles.documentName}>Registro Geral (RG)</p>
-                        <img src="src/images/rg.png" className={styles.docRg} alt="Documento RG"></img>
-                        <input type="file" id="fileRg" name="arquivo1" accept=".pdf, .doc, .docx" />
-                    </div>
-                    <div className={styles.documentWrapper}>
-                        <p className={styles.documentName}>Comprovante de matrícula</p>
-                        <img src="src/images/comprovantedematricula.png" className={styles.docRg} alt="Comprovante de matrícula"></img>
-                        <input type="file" id="fileMatricula" name="arquivo2" accept=".pdf, .doc, .docx" />
-                    </div>
-                    <div className={styles.documentWrapper}>
-                        <p className={styles.documentName}>Comprovante de residencia</p>
-                        <img src="src/images/comprovantederesidencia.png" className={styles.docRg} alt="Comprovante de Matrícula"></img>
-                        <input type="file" id="fileResidencia" name="arquivo3" accept=".pdf, .doc, .docx" />
-                    </div>  
-                </div>
-                </form>
+
                 <section className={styles.dadosSection}>
                     <h1>Dados do usuário</h1>
                     
@@ -33,8 +15,31 @@ function DocumentosPage(){
                     <p className={styles.labelDado}>Endereço:</p>
                     <p className={styles.labelDado}>Instituição:</p>
                     <p className={styles.labelDado}>Cidade:</p>
-                    <p className={styles.labelDado}>Tipo sanguíneo:</p>
+                    <label htmlFor="select" className={styles.selectLabel}>Tipo Sanguíneo
+                        <select className={styles.selectInput}>
+                            <option>A</option>
+                        </select>
+                    </label>
                 </section>
+                    <form action="/upload" method="post" enctype="multipart/form-data">
+                    <div className={styles.mainContent}>
+                        <div className={styles.documentWrapper}>
+                            <p className={styles.documentName}>Registro Geral (RG)</p>
+                            <img src="src/images/rg.png" className={styles.docRg} alt="Documento RG"></img>
+                            <input type="file" id="fileRg" name="arquivo1" accept=".pdf, .doc, .docx" />
+                        </div>
+                        <div className={styles.documentWrapper}>
+                            <p className={styles.documentName}>Comprovante de matrícula</p>
+                            <img src="src/images/comprovantedematricula.png" className={styles.docRg} alt="Comprovante de matrícula"></img>
+                            <input type="file" id="fileMatricula" name="arquivo2" accept=".pdf, .doc, .docx" />
+                        </div>
+                        <div className={styles.documentWrapper}>
+                            <p className={styles.documentName}>Comprovante de residencia</p>
+                            <img src="src/images/comprovantederesidencia.png" className={styles.docRg} alt="Comprovante de Matrícula"></img>
+                            <input type="file" id="fileResidencia" name="arquivo3" accept=".pdf, .doc, .docx" />
+                        </div>  
+                    </div>
+                </form>
             </div>
         </>
     )
